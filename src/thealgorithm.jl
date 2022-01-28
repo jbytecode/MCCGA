cb1ce9731930a7203176dc13e9d0cbac648b8b01
jbytecode <mhsatman@gmail.com>
2022-01-28 17:46:54 +0300

    update return disc

--- src/thealgorithm.jl
+++ src/thealgorithm.jl
@@ -58,7 +58,7 @@ function mccga(;
                     probvector[i] = 1
                 elseif probvector[i] < 0
                     probvector[i] = 0
-                end 
+                end
             end
         end
     end
@@ -68,14 +68,16 @@ function mccga(;
 
     local_result = Optim.optimize(costfunction, initial_solution, Optim.NelderMead())
 
-    resultdict = Dict()
-    resultdict["initial_prob_vector"] = initial_probvector
-    resultdict["final_prob_vector"] = sampledvector
-    resultdict["iterations"] = numiters
-    resultdict["initial_solution"] = floats(sampledvector)
-    resultdict["final_solution"] = local_result.minimizer
-    resultdict["initial_minimum"] = costfunction(floats(sampledvector))
-    resultdict["final_minimum"] = costfunction(local_result.minimizer)
-    
+    resultdict = Dict(
+        "initial_prob_vector" => initial_probvector,
+        "final_prob_vector" => sampledvector,
+        "iterations" => numiters,
+        "initial_solution" => floats(sampledvector),
+        "final_solution" => local_result.minimizer,
+        "initial_minimum" => costfunction(floats(sampledvector)),
+        "final_minimum" => costfunction(local_result.minimizer),
+    )
+
+
     return resultdict
 end
